e4d89712119b05a1a6b1963243bbfb334f6b5214
AndoFalimanantsoa <99736626+Ando181002@users.noreply.github.com>
2023-10-10 15:56:24 +0300

    pdf

---
+++ resources/views/Admin/Pdf.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple de PDF</title>
+</head>
+<body>
+    <h1>Contenu du PDF</h1>
+    <p>Ceci est un exemple de contenu pour le PDF.</p>
+</body>
+</html>
